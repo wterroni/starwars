e5f7e05fceceaae414abafb18ae8c44e31add973
welli <wellington.terroni@outlookc.om>
2023-04-13 15:47:23 -0300

    Inclui testes view model

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Esse projeto está usando arquitetura em MVVM, koin como injeção de dependencia, linguagem 100% em 
 kotlin e layouts utilizando Constraint layout
 
-Além dos fluxos pedidos (tela com categorias permitindo salvar os favoritos, detalhe com os 
-detalhes do item selecionado com campo para filtrar os itens), tamb~em foi desenvolvido loading em
-shimmer, custom view para exibição de erros genéricos, botão flutuante que muda o layout da 
-recycler view entre grid e lista.
+Esse app inspirado em star wars possui: tela com categorias permitindo salvar os favoritos via 
+shared preferences, detalhe da categoria exibindo as informações e permitindo realizar um filtro 
+pelo nome, loading em shimmer, custom view para exibição de erros genéricos, botão flutuante que 
+muda o layout da recycler view entre grid e lista.
--- app/src/test/kotlin/com/example/starwars/categories/presentation/CategoriesViewModelTest.kt
+++ app/src/test/kotlin/com/example/starwars/categories/presentation/CategoriesViewModelTest.kt
@@ -1,11 +1,12 @@
 package com.example.starwars.categories.presentation
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.example.starwars.categories.domain.CategoriesInteractor
 import com.example.starwars.categories.domain.model.Category
+import com.example.starwars.categories.mockArrayCategory
 import com.example.starwars.categories.mockCategory
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
@@ -18,6 +19,14 @@ import org.junit.Test
 @ExperimentalCoroutinesApi
 class CategoriesViewModelTest {
     private val interactor: CategoriesInteractor = mockk()
+    private lateinit var viewModel: CategoriesViewModel
+
+    private lateinit var categoriesOb: Observer<List<Category>>
+    private lateinit var loadingOB: Observer<Boolean>
+    private lateinit var favoriteCategoryListOb: Observer<List<Category>>
+    private lateinit var categoriesExceptionOb: Observer<Throwable>
+
+    private val mockError: Throwable = spyk(Throwable())
 
     @Before
     fun setup() {
@@ -29,6 +38,101 @@ class CategoriesViewModelTest {
     val coroutineRule = InstantTaskExecutorRule()
 
     private fun setupViewModel() {
+        viewModel = CategoriesViewModel(interactor)
+
+        categoriesOb = spyk()
+        loadingOB = spyk()
+        favoriteCategoryListOb = spyk()
+        categoriesExceptionOb = spyk()
+
+        viewModel.categoriesOb.observeForever(categoriesOb)
+        viewModel.loadingOB.observeForever(loadingOB)
+        viewModel.favoriteCategoryListOb.observeForever(favoriteCategoryListOb)
+        viewModel.categoriesExceptionOb.observeForever(categoriesExceptionOb)
+    }
+
+
+    @Test
+    fun `get category return success`() = runBlocking {
+        prepareGetCategories()
+        setupViewModel()
+
+        verify {
+            categoriesOb.onChanged(any())
+            favoriteCategoryListOb.onChanged(any())
+            loadingOB.onChanged(false)
+        }
+    }
+
+    @Test
+    fun `get category return error`() = runBlocking {
+        every {
+            runBlocking {
+                interactor.getCategories()
+            }
+        } throws mockError
+
+        setupViewModel()
+
+        verify {
+            categoriesExceptionOb.onChanged(mockError)
+            loadingOB.onChanged(false)
+        }
+    }
+
+    @Test
+    fun `retry categories return success`() = runBlocking {
+        prepareGetCategories()
+        setupViewModel()
+
+        viewModel.retryCategories()
+
+        verifyOrder {
+            loadingOB.onChanged(true)
+            loadingOB.onChanged(false)
+        }
+
+        verify {
+            categoriesOb.onChanged(any())
+            favoriteCategoryListOb.onChanged(any())
+        }
+    }
+
+    @Test
+    fun `save favorite return success`() = runBlocking {
+        every { interactor.saveFavorite(mockCategory) } just Runs
+        every { interactor.updateLocalFavorites(mockArrayCategory) } returns mockArrayCategory
+
+        prepareGetCategories()
+
+        setupViewModel()
+
+        viewModel.saveFavorite(mockCategory)
+
+        verify {
+            categoriesOb.onChanged(any())
+            favoriteCategoryListOb.onChanged(any())
+        }
+    }
+
+    @Test
+    fun `remove favorite return success`() = runBlocking {
+        every { interactor.removeFavorite(mockCategory) } just Runs
+        every { interactor.updateLocalFavorites(mockArrayCategory) } returns mockArrayCategory
+
+        prepareGetCategories()
+
+        setupViewModel()
+
+        viewModel.removeFavorite(mockCategory)
+
+        verify {
+            categoriesOb.onChanged(any())
+            favoriteCategoryListOb.onChanged(any())
+        }
+    }
+
+    private fun prepareGetCategories() {
         val response = ArrayList<Category>()
         response.add(mockCategory)
         every {
@@ -38,18 +142,4 @@ class CategoriesViewModelTest {
         } returns response
     }
 
-    /*
-    @Test
-    fun `get category return success`() = runBlocking {
-        setupViewModel()
-        val viewModel = CategoriesViewModel(interactor)
-
-        Assert.assertNotNull(viewModel.categoriesOb.value)
-        Assert.assertTrue(viewModel.categoriesOb.value?.size == 2)
-        Assert.assertNotNull(viewModel.favoriteCategoryListOb.value)
-        Assert.assertTrue(viewModel.favoriteCategoryListOb.value?.size == 1)
-
-    }
-
-     */
 }
